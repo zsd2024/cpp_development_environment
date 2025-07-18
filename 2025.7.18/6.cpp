9bc820142fe01aba75033dc1096e017e9b47e8a6
zsd2024 <zsd_2024@outlook.com>
2025-07-18 03:21:21 +0000

    2025年7月18日 11:21

--- 2025.7.18/6.cpp
+++ 2025.7.18/6.cpp
@@ -1,20 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m;
-int a[101][2001];
-vector<int> v;
-void dfs(int d)
-{
-	if (d == n + 1)
-	{
-	}
-}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> n >> m;
-	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= m; ++j)
-			cin >> a[i][j];
+	cout << "742";
 }
