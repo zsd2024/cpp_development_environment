e0ac3fd00b9d2eed4825e130a1b3d6b1ea3f51fa
zsd2024 <zsd_2024@outlook.com>
2025-07-21 04:56:46 +0000

    2025年7月21日 12:56

--- 2025.7.21/P1265.cpp
+++ 2025.7.21/P1265.cpp
@@ -1,8 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int n;
+struct pos
+{
+	int x, y;
+} city[5001];
+inline double dis(int a, int b)
+{
+	return sqrt((city[a].x - city[b].x) * (city[a].x - city[b].x) + (city[a].y - city[b].y) * (city[a].y - city[b].y));
+}
+bool is_in_tree[5001];
+int ans;
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
+	cin >> n;
+	for (int i = 1; i <= n; ++i)
+		cin >> city[i].x >> city[i].y;
 }
