e478da61ae4cc20ff5a92171bf1e12bac09ae57d
zsd2024 <zsd_2024@outlook.com>
2025-07-22 07:19:13 +0000

    2025年7月22日 15:19

--- 2025.7.22/1.cpp
+++ 2025.7.22/1.cpp
@@ -4,9 +4,9 @@ int n, l, r;
 int main()
 {
 	cin >> n >> l >> r;
-	if (r - l >= n - 1)
+	if (r - l /*可以拿的糖果范围*/ >= n - 1 /*最多能拿到的奖励数量*/) // 如果范围更大，那么一定可以得到最多的奖励
 		cout << n - 1;
-	else if (l % n + (r - l) >= n - 1)
+	else if (l % n + (r - l) >= n - 1) // 如果恰好能得到最多奖励的糖果数量在范围内
 		cout << n - 1;
 	else
 		cout << l % n + (r - l);
