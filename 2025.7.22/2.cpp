94d757a289ae16140ce67c96430e575085f9e4af
zsd2024 <zsd_2024@outlook.com>
2025-07-22 07:48:37 +0000

    2025年7月22日 15:48

--- 2025.7.22/2.cpp
+++ 2025.7.22/2.cpp
@@ -1,19 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, q;
-struct point
-{
-	int x, data;
-} a[8001], t[8001];
-bool cmp(point l, point r)
-{
-	return l.data < r.data || (l.data == r.data && l.x < r.x);
-}
+int a[8001];
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	cin >> n >> q;
 	for (int i = 1; i <= n; ++i)
-		a[i].x = i, cin >> a[i].data;
+		cin >> a[i];
 	for (int i = 1; i <= q; ++i)
 	{
 		int x;
@@ -22,17 +17,17 @@ int main()
 		{
 			int x, y;
 			cin >> x >> y;
-			a[x].data = y;
+			a[x] = y;
 		}
 		else if (x == 2)
 		{
-			memcpy(t, a, sizeof(a));
-			sort(t + 1, t + 1 + n, cmp);
 			int x;
 			cin >> x;
+			int id = n;
 			for (int i = 1; i <= n; ++i)
-				if (t[i].x == x)
-					cout << i << '\n';
+				if (a[i] > a[x])
+					--id;
+			cout << id << '\n';
 		}
 	}
 }
