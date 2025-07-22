484855aac02baccebb39ee26090928fab13e4993
zsd2024 <zsd_2024@outlook.com>
2025-07-22 08:09:55 +0000

    2025年7月22日 16:09

--- 2025.7.22/3.cpp
+++ 2025.7.22/3.cpp
@@ -23,142 +23,84 @@ void checks(string st, comp &com)
 		int e = atoi(matches[5].str().c_str());
 		if (a < 0 && a > 127)
 		{
-			cout << "ERR";
-			exit(0);
+			cout << "ERR\n";
+			return;
 		}
 		if (b < 0 && b > 127)
 		{
-			cout << "ERR";
-			exit(0);
+			cout << "ERR\n";
+			return;
 		}
 		if (c < 0 && c > 127)
 		{
-			cout << "ERR";
-			exit(0);
+			cout << "ERR\n";
+			return;
 		}
 		if (d < 0 && d > 127)
 		{
-			cout << "ERR";
-			exit(0);
+			cout << "ERR\n";
+			return;
 		}
 		if (e < 0 && e > 65535)
 		{
-			cout << "ERR";
-			exit(0);
+			cout << "ERR\n";
+			return;
 		}
 		com.a = a;
 		com.b = b;
 		com.c = c;
 		com.d = d;
 		com.e = e;
+		cout << "OK\n";
 	}
+	else
+		cout << "ERR\n";
 }
 void checkc(string st)
 {
 	comp com;
+	regex reg(R"raw((0|(?:[1-9][0-9]*)).(0|(?:[1-9][0-9]*)).(0|(?:[1-9][0-9]*)).(0|(?:[1-9][0-9]*)):(0|(?:[1-9][0-9]*)))raw");
+	std::smatch matches;
+	if (regex_match(st, matches, reg))
 	{
-		if (st[0] == '0' && isdigit(st[1]))
+		int a = atoi(matches[1].str().c_str());
+		int b = atoi(matches[2].str().c_str());
+		int c = atoi(matches[3].str().c_str());
+		int d = atoi(matches[4].str().c_str());
+		int e = atoi(matches[5].str().c_str());
+		if (a < 0 && a > 127)
 		{
 			cout << "ERR\n";
 			return;
 		}
-		int x = st.find(".");
-		if (x == string::npos)
+		if (b < 0 && b > 127)
 		{
 			cout << "ERR\n";
 			return;
 		}
-		int a = stoi(st.substr(0, x));
-		if (a > 255)
+		if (c < 0 && c > 127)
+		{
+			cout << "ERR\n";
+			return;
+		}
+		if (d < 0 && d > 127)
+		{
+			cout << "ERR\n";
+			return;
+		}
+		if (e < 0 && e > 65535)
 		{
 			cout << "ERR\n";
 			return;
 		}
 		com.a = a;
-		st.erase(0, x + 1);
-	}
-
-	{
-		if (st[0] == '0' && isdigit(st[1]))
-		{
-			cout << "ERR\n";
-			return;
-		}
-		int x = st.find(".");
-		if (x == string::npos)
-		{
-			cout << "ERR\n";
-			return;
-		}
-		int b = stoi(st.substr(0, x));
-		if (b > 255)
-		{
-			cout << "ERR\n";
-			return;
-		}
 		com.b = b;
-		st.erase(0, x + 1);
-	}
-
-	{
-		if (st[0] == '0' && isdigit(st[1]))
-		{
-			cout << "ERR\n";
-			return;
-		}
-		int x = st.find(".");
-		if (x == string::npos)
-		{
-			cout << "ERR\n";
-			return;
-		}
-		int c = stoi(st.substr(0, x));
-		if (c > 255)
-		{
-			cout << "ERR\n";
-			return;
-		}
 		com.c = c;
-		st.erase(0, x + 1);
-	}
-
-	{
-		if (st[0] == '0' && isdigit(st[1]))
-		{
-			cout << "ERR\n";
-			return;
-		}
-		int x = st.find(":");
-		if (x == string::npos)
-		{
-			cout << "ERR\n";
-			return;
-		}
-		int d = stoi(st.substr(0, x));
-		if (d > 255)
-		{
-			cout << "ERR\n";
-			return;
-		}
 		com.d = d;
-		st.erase(0, x + 1);
-	}
-
-	{
-		if (st[0] == '0' && isdigit(st[1]))
-		{
-			cout << "ERR\n";
-			return;
-		}
-		int e = stoi(st);
-		if (e > 65535)
-		{
-			cout << "ERR\n";
-			return;
-		}
 		com.e = e;
 	}
-
+	else
+		cout << "ERR\n";
 	for (int i = 1; i <= wz; ++i)
 		if (com == a[i])
 		{
@@ -169,7 +111,6 @@ void checkc(string st)
 }
 int main()
 {
-	checks("1.2.3.4:5", a[++wz]);
 	cin >> n;
 	for (int i = 1; i <= n; ++i)
 	{
