2031df399d17e4b2049de5099a4a3faf806f8057
zsd2024 <zsd_2024@outlook.com>
2025-07-24 02:35:56 +0000

    2025年7月24日 10:35

--- 2025.7.24/4.cpp
+++ 2025.7.24/4.cpp
@@ -3,6 +3,23 @@ using namespace std;
 int n, m, k;
 int dis[10001][100];
 vector<pair<int, int>> g[10001];
+int try_to_go(int start_time)
+{
+	memset(dis, 0x3f, sizeof(dis));
+	dis[1][0] = 0;
+	for (int i = 1; i <= n; ++i)
+		for (const pair<int, int> &t : g[i])
+		{
+			int v = t.first;
+			int a = t.second;
+			if (a <= dis[i][k - 1])
+				dis[v][0] = min(dis[i][k - 1] + 1, dis[v][0]);
+			for (int j = 1; j < k; ++j)
+				if (a <= dis[i][j - 1])
+					dis[v][j] = min(dis[i][j - 1] + 1, dis[v][j]);
+		}
+	return dis[n][0];
+}
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -24,5 +41,4 @@ int main()
 			for (int j = 1; j < k; ++j)
 				dis[v][j] = min(dis[i][j - 1] + 1, dis[v][j]);
 		}
-	cout << dis[n][0] << '\n';
 }
