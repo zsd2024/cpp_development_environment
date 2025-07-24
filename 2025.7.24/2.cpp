f5722c8691ad2d0c3a2257f506f0e8c5912b7df8
zsd2024 <zsd_2024@outlook.com>
2025-07-24 01:18:00 +0000

    2025年7月24日 09:18

--- 2025.7.24/2.cpp
+++ 2025.7.24/2.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, d;
+int jl, q;
 int v[100001];
 int a[100001];
+vector<int> stations;
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -12,4 +14,23 @@ int main()
 		cin >> v[i];
 	for (int i = 1; i <= n; ++i)
 		cin >> a[i];
+	stations.push_back(1);
+	for (int i = 2, prize = a[1]; i < n; ++i)
+		if (a[i] < prize)
+		{
+			prize = a[i];
+			stations.push_back(i);
+		}
+	stations.push_back(n);
+	for (size_t i = 0; i + 1 < stations.size(); ++i)
+	{
+		int vs = 0;
+		for (int j = stations[i]; j < stations[i + 1]; ++j)
+			vs += v[j];
+		int jyl = ((vs - jl) + (d - 1)) / d;
+		cout << stations[i] << ' ' << vs << ' ' << stations[i + 1] << ' ' << jyl << '\n';
+		q += jyl * a[i];
+		jl += d * jyl;
+	}
+	cout << q;
 }
