4b273f18fb552a4c03433d35a1da6a0cdd265996
zsd2024 <zsd_2024@outlook.com>
2025-07-24 02:20:30 +0000

    2025年7月24日 10:20

---
+++ 2025.7.24/4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, k;
+int dis[10001][] vector<pair<int, int>> g[10001];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m >> k;
+	for (int i = 1; i <= n; ++i)
+	{
+		int u, v, a;
+		g[u].push_back({v, a});
+	}
+}
