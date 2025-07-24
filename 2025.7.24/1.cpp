836791190d90898525efe5a895ec7ba35714288f
zsd2024 <zsd_2024@outlook.com>
2025-07-24 00:55:04 +0000

    2025年7月24日 08:55

--- 2025.7.24/1.cpp
+++ 2025.7.24/1.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
+int ans1, ans2;
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> n;
+	while (n)
+	{
+		n -= n / 3;
+		if (n % 3 == 1 && !ans1)
+			ans1 = ans1;
+		++ans2;
+	}
+	cout << ans1 << ' ' << ans2 << '\n';
 }
